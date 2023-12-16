a51fe4c38b842c91544fefbdff95ff81d0a4435a
João Corsi <jjcorsif@hotmail.com>
2023-12-16 16:48:53 -0300

    first commit

---
+++ src/test/java/com/jjcorsif/perfectNumbers/PerfectNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjcorsif.perfectNumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
